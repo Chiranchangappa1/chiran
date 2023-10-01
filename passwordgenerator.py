bb0b787f90d623bb2dac8b20a3ceab3d83be5cd3
Chiranchangappa1 <117272010+Chiranchangappa1@users.noreply.github.com>
2023-10-01 16:35:04 +0530

    Add files via upload

---
+++ passwordgenerator.py
@@ -0,0 +1,73 @@
+import random
+import string
+
+
+# 1st password
+
+print("<<<<GET YOUR ONE-TIME SECURITY PASSWORD HERE>>>>")
+total = string.ascii_letters + string.digits
+length = 10
+password = "".join(random.sample(total, length))
+print("YOUR HIGH SECURITY PASSWORD IS:")
+print("*******************************")
+print(password)
+
+# 2nd password
+
+total = string.ascii_letters + string.digits
+length = 10
+password = "".join(random.sample(total, length))
+print(password)
+
+# 3rd password
+
+total = string.ascii_letters + string.digits
+length = 10
+password = "".join(random.sample(total, length))
+print(password)
+print("*******************************")
+
+# 4th super security password
+
+total = string.ascii_letters + string.digits + string.punctuation
+length = 10
+password = "".join(random.sample(total, length))
+print("SUPER SECURITY PASSWORD IS:")
+print("*******************************")
+print(password)
+import random
+import string
+
+# 1st password
+print("<<<<GET YOUR ONE-TIME SECURITY PASSWORD HERE>>>>")
+total = string.ascii_letters + string.digits
+length = 10
+password = "".join(random.sample(total, length))
+print("YOUR HIGH SECURITY PASSWORD IS:")
+print("*******************************")
+print(password)
+
+# 2nd password
+
+total = string.ascii_letters + string.digits
+length = 10
+password = "".join(random.sample(total, length))
+print(password)
+
+# 3rd password
+
+total = string.ascii_letters + string.digits
+length = 10
+password = "".join(random.sample(total, length))
+print(password)
+print("*******************************")
+
+# 4th super security password
+
+total = string.ascii_letters + string.digits + string.punctuation
+length = 10
+password = "".join(random.sample(total, length))
+print("SUPER SECURITY PASSWORD IS:")
+print("*******************************")
+print(password)
+
